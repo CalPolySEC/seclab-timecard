3c04ee9a0bf228c76ffb259b3f14f8da00cb7e5a
Nick Frost <nickfrostatx@gmail.com>
2017-02-14 18:08:53 -0800

    Deploy with Travis

---
+++ travis/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+eval "$(ssh-agent -s)"
+chmod 700 travis
+openssl aes-256-cbc -K $encrypted_509c4c4decd3_key -iv $encrypted_509c4c4decd3_iv -in deploy_rsa.enc -out travis/deploy_rsa -d
+chmod 600 travis/deploy_rsa
+ssh-add travis/deploy_rsa
+mkdir -m 700 -p ~/.ssh
+echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
+git remote add deploy "git@thewhitehat.club:seclab-timecard"
+PUSH="$(git push deploy 2>&1)"
+echo "$PUSH"
+echo "$PUSH" | grep -q "SUCCESS"

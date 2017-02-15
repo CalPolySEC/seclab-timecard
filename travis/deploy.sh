2c55dc3c0e58f029b2c488748acb5bf0868f1d1e
Nick Frost <nickfrostatx@gmail.com>
2017-02-14 23:33:10 -0800

    Cycle keys

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 eval "$(ssh-agent -s)"
 chmod 700 travis
-openssl aes-256-cbc -K $encrypted_509c4c4decd3_key -iv $encrypted_509c4c4decd3_iv -in deploy_rsa.enc -out travis/deploy_rsa -d
+openssl aes-256-cbc -K $encrypted_509c4c4decd3_key -iv $encrypted_509c4c4decd3_iv -in travis/deploy_rsa.enc -out travis/deploy_rsa -d
 chmod 600 travis/deploy_rsa
 ssh-add travis/deploy_rsa
 mkdir -m 700 -p ~/.ssh
